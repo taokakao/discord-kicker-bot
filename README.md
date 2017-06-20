794ba74a22b9cf5d3c09864deb99b23a671bf4f6
Andrei Talmachou <andrei.talmachou@optibet.ee>
2017-06-20 17:31:04 +0300

    eslint, node_env and bot init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # discord-kicker-bot
 Kicker bot for Discord
+
+### ENV VARS
+
+`PORT` - Port to run server on (Default: 3333)
+
+`TOKEN` - Bot user OAuth2 token
+
+`SECRET` - Discord app secret
+
+`CHANNEL` - Text channel name to post to (Default: general)
---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+const token = process.env.TOKEN;
+const secret = process.env.SECRET;
+const channelName = process.env.CHANNEL || 'general';
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', (message) => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login(token);
